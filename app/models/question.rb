476cb67919dcbb5e82a71531a8a1d3619ca61430
Ivan Belitskii <belitskii@gmail.com>
2018-04-02 17:56:07 +0300

    Apply suggestions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  validates :title, presence: true
-  validates :body, presence: true
+
+  validates :title, :body, presence: true
 end
