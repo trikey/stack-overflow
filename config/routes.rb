a8bc6a8d855f38771ac6400b23e2da4dfbf54028
Ivan Belitskii <belitskii@gmail.com>
2018-04-09 10:42:47 +0300

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :questions, shallow: true do
     resources :answers, except: [:index, :show]
   end
+
+  root to: "questions#index"
 end
