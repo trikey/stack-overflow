04166c8471c7213db7a15627cb2f0538da087565
ivan <belitskii@gmail.com>
2018-04-25 18:38:19 +0300

    Add attaching multiple files to answer test

--- spec/features/answers/attach_files_to_answer_spec.rb
+++ spec/features/answers/attach_files_to_answer_spec.rb
@@ -17,10 +17,26 @@ feature 'Attach files to answers', %q(
   scenario 'User adds file when asks the question', js: true do
     fill_in 'Your answer', with: 'Some booody'
     attach_file 'File', "#{Rails.root}/spec/fixtures/img.jpg"
+
+    click_on 'add file'
+
+    within '.nested_fields:nth-child(2)' do
+      attach_file 'File', "#{Rails.root}/spec/fixtures/img2.jpg"
+    end
+
+    click_on 'add file'
+
+    within '.nested_fields:nth-child(3)' do
+      attach_file 'File', "#{Rails.root}/spec/fixtures/img3.jpg"
+      click_on 'remove file'
+    end
+
     click_on 'Create Answer'
 
     within '.answers' do
       expect(page).to have_link 'img.jpg', href: '/uploads/attachment/file/1/img.jpg'
+      expect(page).to have_link 'img2.jpg', href: '/uploads/attachment/file/2/img2.jpg'
+      expect(page).to_not have_link 'img3.jpg', href: '/uploads/attachment/file/3/img3.jpg'
     end
   end
 end
